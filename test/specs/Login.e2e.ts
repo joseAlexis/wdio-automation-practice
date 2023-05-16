acb630816537640674845496a2c1d288370cd1e4
Jose Bolanos <joseAlexis>
2023-05-16 23:17:06 +0200

    Adding hooks to test

--- test/specs/Login.e2e.ts
+++ test/specs/Login.e2e.ts
@@ -4,28 +4,21 @@ import { generateUser } from '../dataProviders/UserDataProvider.ts';
 import { createUserAPI, removeUserAPI } from '../api/user.api.ts';
 
 describe("User test suite", () => {
-    
     before(async function () {
+        this.user = generateUser();
+        console.log(this.user);
+        await createUserAPI(this.user);
+    });
+
+    after(async function () {
+        await removeUserAPI({ email: this.user.email, password: this.user.password });
+    });
+
+    it('Should login with valid credentials', async function () {
         await LoginPage.open();
-
-
-    })
-
-    it('Should login with valid credentials', async () => {
         await expect(LoginPage.signInTitle).toBeDisplayed();
-        const data = generateUser();
-        console.log(data);
 
-        // Create user for test validation
-        const response = await createUserAPI(data);
-        expect(response.data.responseCode).toBe(201);
-        expect(response.data.message).toBe('User created!');
-
-        await LoginPage.signIn({ email: data.email, password: data.password });
-        await expect(HomePage.menuItem(10)).toHaveTextContaining(`Logged in as ${data.name}`);
-
-        const responseDelete = await removeUserAPI({ email: data.email, password: data.password });
-        expect(responseDelete.data.responseCode).toBe(200);
-        expect(responseDelete.data.message).toBe('Account deleted!')
+        await LoginPage.signIn({ email: this.user.email, password: this.user.password });
+        await expect(HomePage.menuItem(10)).toHaveTextContaining(`Logged in as ${this.user.name}`);
     })
 });
